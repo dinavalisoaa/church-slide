0756ed3411767f694043a521e2ce9c055dae0786
Dina Valisoa <randriamifidydina@gmail.com>
2024-09-24 22:47:38 +0300

    [ADD] creation of And component

---
+++ src/components/Andininy.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { RevealHandle, RevealSlides } from "./Reveal";
+import { BiLogoGithub } from "react-icons/bi";
+
+import "../App.css";
+
+function Andininy(props) {
+  const data = props.data;
+  const and = props.and;
+  const data_plot = data.split("\n");
+  const return_values = data_plot.map((element, index) => {
+    return <p>{element}</p>;
+  });
+  console.log(return_values);
+  return return_values;
+}
+export default Andininy;
