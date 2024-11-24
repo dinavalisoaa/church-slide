65ad72eb59bb5a9469ba42aa2286000038221024
Dina Valisoa <randriamifidydina@gmail.com>
2024-11-24 04:43:19 +0300

    [Back-Main] desabled SQL query Log

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-
+use ::entity::{types, types::Entity as Types};
 use entity::post;
+use migration::sea_orm::EntityTrait;
 use migration::{Migrator, MigratorTrait};
 use serde::{Deserialize, Serialize};
-use migration::sea_orm::EntityTrait;
+use service::sea_orm::{Database, DatabaseConnection};
+use service::TypesService;
 use std::env;
 use std::fs;
-use service::{
-    sea_orm::{Database, DatabaseConnection},
-    Mutation as MutationCore, Query as QueryCore,
-};
-
-
-
 #[tokio::main]
 async fn main() {
     env::set_var("RUST_LOG", "debug");
-    tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt::init();
 
     dotenvy::dotenv().ok();
-    
+
     let home_dir = match tauri::api::path::home_dir() {
         Some(val) => val,
         None => panic!("Could not get home directory"),
@@ -32,15 +26,14 @@ async fn main() {
     // }1.8.0
 
     // let db_url = "sqlite://".to_string() + data_dir.to_str().unwrap() + "/db.sqlite?mode=rwc";
+    // let db_url = "sqlite://C://Users//DINA//.church-slide//data//db.sqlite";
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
-    // let db_url = "sqlite://db.sqlite"; 
+    // let db_url = "sqlite://db.sqlite";
     let conn = Database::connect(db_url)
         .await
         .expect("Database connection failed");
-    // Migrator::up(&conn, None).await.unwrap();
+    Migrator::up(&conn, None).await.unwrap();
 
-
-  
     // let posts = QueryCore::find_all(&conn)
     //     .await
     //     .expect("Cannot find posts in page");
@@ -55,24 +48,66 @@ async fn main() {
     //     .await
     //     .expect("could not insert post");
 
-    
-
     let state = AppState { conn };
 
     tauri::Builder::default()
+        // .filter_level(log::LevelFilter::Info)
+        // .filter_module("sqlx:query",log::LevelFilter::Off)
         .manage(state)
         .invoke_handler(tauri::generate_handler![
-            greet, 
-            create_post,
-            update_post,
-            delete_post,
-            list_posts,
-        ])
+            save_types,
+            retrieve_types,
+            retrieve_type_by_id 
+            ])
+
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+#[tauri::command]
+async fn save_types(
+    state: tauri::State<'_, AppState>,
+    form: types::Model,
+) -> Result<FlashData, ()> {
+    let _ = &state.conn;
+    TypesService::save(&state.conn, form)
+        .await
+        .expect("could not insert save");
 
+    let data = FlashData {
+        kind: "success".to_owned(),
+        message: "Save succcessfully added".to_owned(),
+    };
+    Ok(data)
+}
+
+#[tauri::command]
+async fn retrieve_types(
+    state: tauri::State<'_, AppState>,
+) -> Result<Vec<types::Model>, ()> {
+    let _ = &state.conn;
+    let data=TypesService::find_all(&state.conn)
+        .await
+        .expect("could not insert save");
+ 
+    Ok(data)
+}
+
+
+#[tauri::command]
+async fn retrieve_type_by_id(
+    state: tauri::State<'_, AppState>,
+    id:i32
+)-> Result<Option<types::Model>, ()>{
+    let _ = &state.conn;
+    let data = TypesService::find_by_id(&state.conn,id)
+        .await
+        .expect("could not insert save");
+        Ok(data)
+}
+
+
+/*
 #[tauri::command]
 async fn create_post(state: tauri::State<'_, AppState>, form: post::Model) -> Result<FlashData, ()> {
     let _ = &state.conn;
@@ -148,7 +183,7 @@ async fn list_posts(
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-
+     */
 
 #[derive(Clone)]
 struct AppState {
