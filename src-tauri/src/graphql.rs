c35467ca9e37c190c1f0514b7df18216c616c315
Dina Valisoa <randriamifidydina@gmail.com>
2024-12-04 16:54:00 +0300

    graphql : add simple step

--- src-tauri/src/graphql.rs
+++ src-tauri/src/graphql.rs
@@ -1,40 +1,25 @@
-use async_graphql::{Context, EmptyMutation, Object, Schema, EmptySubscription,SimpleObject, Subscription};
+use async_graphql::{Context, EmptyMutation, Object,MergedObject,MergedSubscription, Schema, EmptySubscription,SimpleObject, Subscription};
+// use async_graphql::{Object, MergedObject, MergedSubscription, Schema, SimpleObject};
 use async_graphql_tide::graphql;
 use tide::Server;
-
-#[derive(SimpleObject)]
-struct Book {
-    id: i32,
-    title: String,
-    author: String,
-}
-
-pub struct QueryRoot;
-
-#[Object]
-impl QueryRoot {
-    async fn books(&self) -> Vec<Book> {
-        vec![
-            Book {
-                id: 1,
-                title: "1984".to_string(),
-                author: "George Orwell".to_string(),
-            },
-            Book {
-                id: 2,
-                title: "Brave New World".to_string(),
-                author: "Aldous Huxley".to_string(),
-            },
-        ]
-    }
-}
+use ::slide_graphql::SlideSchema;
+use ::slide_graphql::{
+        Mutation, Query, Subscription
+};
+use repository::DbConnection;
 
 pub async fn create_graphql_server() -> tide::Result<()> {
-    let schema = Schema::build(QueryRoot, EmptyMutation,EmptySubscription)
-        .finish();
+
+    let db = DbConnection::new().await;
+    let schema = SlideSchema::build(
+        Query::default(),
+        Mutation::default(),
+        Subscription::default(),
+    ).data(db)
+    .finish();
 
     let mut app = Server::new();
-    app.at("/graphql").post(async_graphql_tide::graphql(schema));
+    app.at("/graphql").post(graphql(schema));
     println!("GraphQL server running on http://localhost:8080/graphql");
     app.listen("127.0.0.1:8080").await?;
     Ok(())
