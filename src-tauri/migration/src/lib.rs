6b6791d93a86fc4c29b6ce804236b7e3447b4659
Dina Valisoa Randriamifidy <randriamifidydina@gmail.com>
2025-04-17 16:47:26 +0300

    feature(AuthorService) : UI and Back

--- src-tauri/migration/src/lib.rs
+++ src-tauri/migration/src/lib.rs
@@ -1,7 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20241021_080046_create_table_migration;
-
+mod m20241021_insert_table;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -10,6 +10,7 @@ impl MigratorTrait for Migrator {
         vec![
             // Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20241021_080046_create_table_migration::Migration),
+            Box::new(m20241021_insert_table::Migration)
         ]
     }
 }
